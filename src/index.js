d44453210f1231fd2613f022599761af155ebc8b
Nurad Namazov <nurad9689@rambler.ru>
2023-05-29 13:24:52 +0300

    Переписал вывод движка игр

--- src/index.js
+++ src/index.js
@@ -4,23 +4,22 @@ export const roundsNumber = 3;
 
 const runGameLogic = (generateRound, description) => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!`);
   console.log(description);
   for (let i = 0; i < roundsNumber; i += 1) {
-    const [question, answer] = generateRound();
+    const [question, corrAnswer] = generateRound();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'`);
-      console.log(`Let's try again, ${name}`);
+
+    if (corrAnswer !== userAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${corrAnswer}'.`);
+      console.log(`Let's try again, ${username}!`);
       return;
     }
+    console.log('Correct!');
   }
-
-  console.log(`Congratulations, ${name}!`);
+  console.log(`Congratulations, ${username}!`);
 };
 
 export default runGameLogic;
