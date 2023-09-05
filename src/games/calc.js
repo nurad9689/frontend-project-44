504e16d4c4bd80f01268dec6c16190e7dca79b44
Nurad Namazov <nurad9689@rambler.ru>
2023-09-05 17:44:20 +0300

    back to origin of calc.js file

--- src/games/calc.js
+++ src/games/calc.js
@@ -33,4 +33,4 @@ const generateRound = () => {
   return [question, correctAnswer];
 };
 
-export default runCalcGame = () => runGameLogic(generateRound, description);
+export default () => runGameLogic(generateRound, description);
